195dd2718ca907eb08cfb17f68485d5764d8eb57
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2025-08-18 20:39:39 +0300

    Update App.jsx

--- client/MacroMaster/src/App.jsx
+++ client/MacroMaster/src/App.jsx
@@ -3,8 +3,8 @@ import { useDispatch } from "react-redux";
 import Navbar from './partials/Navbar';
 import Footer from './partials/Footer';
 import { setUser } from "./state_manager/userSlice";
-import { getJwtFromCookie } from '../authentication'
-
+import { getJwtFromCookie } from '../authentication';
+import Homepage from './pages/Homepage';
 
 function App() {
   const dispatch = useDispatch();
@@ -19,6 +19,7 @@ function App() {
   return (
     <>
       <Navbar />
+      <Homepage />
       <Footer />
     </>
   );
