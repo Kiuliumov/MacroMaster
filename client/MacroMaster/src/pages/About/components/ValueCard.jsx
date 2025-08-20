c775271c2dabc54fba7089769897698833b821b2
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2025-08-20 19:24:31 +0300

    Update ValueCard.jsx

--- client/MacroMaster/src/pages/About/components/ValueCard.jsx
+++ client/MacroMaster/src/pages/About/components/ValueCard.jsx
@@ -1,12 +1,11 @@
 import React from "react";
-import { commonStyles } from "../../../styles/commonStyles";
 
 export default function ValueCard({ Icon, title, text, color }) {
   return (
-    <div className={commonStyles.card}>
-      {Icon && <Icon className={`${commonStyles.featureIcon} ${color}`} />}
-      <h3 className={commonStyles.featureTitle}>{title}</h3>
-      <p className={commonStyles.featureText}>{text}</p>
+    <div className="bg-white dark:bg-gray-700 p-6 rounded-2xl shadow-md border border-gray-100 dark:border-gray-600">
+      { Icon && <Icon className={`mx-auto h-10 w-10 mb-4 ${color}`} />}
+      <h3 className="text-xl font-semibold mb-2">{title}</h3>
+      <p className="text-gray-700 dark:text-gray-300 text-sm">{text}</p>
     </div>
   );
 }
