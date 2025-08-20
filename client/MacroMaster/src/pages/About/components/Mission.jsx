00c9d360241bef8ca9b41e431f905b02c828cadb
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2025-08-20 19:24:26 +0300

    Update Mission.jsx

--- client/MacroMaster/src/pages/About/components/Mission.jsx
+++ client/MacroMaster/src/pages/About/components/Mission.jsx
@@ -1,11 +1,10 @@
 import React from "react";
-import { commonStyles } from "../../../styles/commonStyles";
 
 export default function Mission() {
   return (
-    <div className={commonStyles.headerWrapper}>
-      <h2 className={commonStyles.pageTitle}>Our Mission</h2>
-      <p className={commonStyles.pageSubtitle}>
+    <div className="mt-16 max-w-2xl text-center">
+      <h2 className="text-3xl font-bold mb-4">Our Mission</h2>
+      <p className="text-gray-700 dark:text-gray-300 text-lg leading-relaxed">
         We aim to empower people to make informed nutritional choices,
         simplify tracking, and achieve their health goals with confidence.
       </p>
