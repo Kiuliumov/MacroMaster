79dd9cc3eb33e8c0614ec9577d2ad71eb0e91dd5
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2025-08-20 18:49:21 +0300

    Update TestimonialCard.jsx

--- client/MacroMaster/src/pages/About/components/TestimonialCard.jsx
+++ client/MacroMaster/src/pages/About/components/TestimonialCard.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import { commonStyles } from "../../../styles/commonStyles";
 
 export default function TestimonialCard({ text, author }) {
   return (
-    <div className="bg-white dark:bg-gray-700 p-6 rounded-2xl shadow-md border border-gray-100 dark:border-gray-600">
+    <div className={commonStyles.card}>
       <p className="text-gray-700 dark:text-gray-300 text-sm mb-2">{text}</p>
       <span className="font-semibold">{author}</span>
     </div>
