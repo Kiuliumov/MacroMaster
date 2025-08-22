6dafaa8dca32e806bc739108f5f2eb4187fc4c05
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2025-08-22 13:35:56 +0300

    Update GuestRoute.jsx

--- client/MacroMaster/src/route_components/GuestRoute.jsx
+++ client/MacroMaster/src/route_components/GuestRoute.jsx
@@ -5,7 +5,7 @@ export default function GuestRoute({ children }) {
   const user = useSelector((state) => state.user.user);
 
   if (user?.token) {
-    return <Navigate to="/dashboard" replace />;
+    return <Navigate to="/" replace />;
   }
 
   return children;
