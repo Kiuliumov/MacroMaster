ce6a78681579bffa8771b760cf4879c053dec53d
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2025-08-24 10:06:23 +0300

    Update CardWrapper.jsx

--- client/MacroMaster/src/pages/auth/components/CardWrapper.jsx
+++ client/MacroMaster/src/pages/auth/components/CardWrapper.jsx
@@ -2,11 +2,9 @@ import { commonStyles } from "../commonStyles";
 
 export default function CardWrapper({ title, children }) {
   return (
-    <div className={commonStyles.container}>
-      <div className={commonStyles.card}>
-        {title && <h1 className={commonStyles.title}>{title}</h1>}
-        {children}
-      </div>
+    <div className={commonStyles.card}>
+      {title && <h1 className={commonStyles.title}>{title}</h1>}
+      {children}
     </div>
   );
 }
