b5f8ae2a6e6b9f0ec248a4c71aee7b1f9d59ead2
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2025-08-24 09:47:13 +0300

    Update About.jsx

--- client/MacroMaster/src/pages/About/About.jsx
+++ client/MacroMaster/src/pages/About/About.jsx
@@ -10,12 +10,22 @@ import { commonStyles } from "../../styles/commonStyles";
 export default function About() {
   return (
     <div className={commonStyles.container}>
-      <Header />
-      <FeaturesGrid />
-      <Mission />
-      <ValuesGrid />
-      <TestimonialsGrid />
-      <CTA />
+      <div
+        className={commonStyles.gradientBlur}
+        style={{
+          background:
+            "linear-gradient(106.89deg, rgba(192, 132, 252, 0.2) 15.73%, rgba(14, 165, 233, 0.6) 15.74%, rgba(232, 121, 249, 0.35) 56.49%, rgba(79, 70, 229, 0.5) 115.91%)",
+        }}
+      />
+
+      <div className="relative max-w-5xl mx-auto space-y-20 flex flex-col items-center w-full">
+        <Header />
+        <FeaturesGrid />
+        <Mission />
+        <ValuesGrid />
+        <TestimonialsGrid />
+        <CTA />
+      </div>
     </div>
   );
 }
