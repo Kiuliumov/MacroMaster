3a744f240a92d5be3f1354a7aca1d149f2c68f5a
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2025-08-24 10:06:26 +0300

    Update RegisterPage.jsx

--- client/MacroMaster/src/pages/auth/RegisterPage.jsx
+++ client/MacroMaster/src/pages/auth/RegisterPage.jsx
@@ -7,17 +7,29 @@ import { commonStyles } from "./commonStyles";
 export default function RegisterPage() {
   const [success, setSuccess] = useState(false);
 
-  return success ? (
-    <SuccessMessage />
-  ) : (
-    <CardWrapper title="Create Account">
-      <RegisterForm onSuccess={() => setSuccess(true)} />
-      <p className={commonStyles.textCenter}>
-        Already have an account?{" "}
-        <a href="/login" className={commonStyles.link}>
-          Log in
-        </a>
-      </p>
-    </CardWrapper>
+  if (success) return <SuccessMessage />;
+
+  return (
+    <div className={commonStyles.container}>
+      <div
+        className={commonStyles.loginGradientBlur} 
+        style={{
+          background:
+            "linear-gradient(106.89deg, rgba(192,132,252,0.3) 15.73%, rgba(14,165,233,0.8) 15.74%, rgba(232,121,249,0.5) 56.49%, rgba(79,70,229,0.7) 115.91%)",
+        }}
+      />
+
+      <div className="relative z-10 w-full max-w-md">
+        <CardWrapper title="Create Account">
+          <RegisterForm onSuccess={() => setSuccess(true)} />
+          <p className={commonStyles.textCenter}>
+            Already have an account?{" "}
+            <a href="/login" className={commonStyles.link}>
+              Log in
+            </a>
+          </p>
+        </CardWrapper>
+      </div>
+    </div>
   );
 }
