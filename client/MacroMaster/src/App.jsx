53423972730b6c98d30c1eeacbab325cc383bb8d
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2025-08-27 14:57:04 +0300

    Update App.jsx

--- client/MacroMaster/src/App.jsx
+++ client/MacroMaster/src/App.jsx
@@ -1,32 +1,19 @@
-import { Routes, Route} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import { Suspense } from "react";
+import routes from "./routes";
 
 import Navbar from "./partials/Navbar";
 import Footer from "./partials/Footer";
 import Toast from "./components/Toast";
 import ScrollToTop from "./components/Scroller";
 import CookieNotice from "./components/CookieNotice";
+import Loader from "./components/Loader";
 
-import Homepage from "./pages/Home/Homepage";
-import About from "./pages/About/About";
-import CareersPage from "./pages/Careers/CareersPage";
-import PricingPage from "./pages/Pricing/PricingPage";
-import PrivacyPolicy from "./pages/PrivacyPolicy";
-import SupportPage from "./pages/SupportPage";
-import FeaturesPage from "./pages/Features/FeaturesPage";
-import DashboardPage from "./pages/DashboardPage";
-
-import LoginPage from "./pages/auth/LoginPage";
-import RegisterPage from "./pages/auth/RegisterPage";
-import ForgotPasswordPage from "./pages/auth/ForgotPasswordPage";
-
-import ProtectedRoute from "./route_components/ProtectedRoute";
-import GuestRoute from "./route_components/GuestRoute";
-import ActivationSuccess from "./pages/auth/components/ActivationSuccess";
-import NotFoundPage from "./pages/NotFoundPage";
 import { useAuth } from "../hooks/useAuth";
 
 function App() {
-	useAuth();
+  useAuth();
+
   return (
     <>
       <ScrollToTop />
@@ -34,53 +21,13 @@ function App() {
       <CookieNotice />
       <Navbar />
 
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/careers" element={<CareersPage />} />
-        <Route path="/pricing" element={<PricingPage />} />
-        <Route path="/support" element={<SupportPage />} />
-        <Route path="/features" element={<FeaturesPage />} />
-        <Route path="/policy" element={<PrivacyPolicy />} />
-
-        <Route
-          path="/login"
-          element={
-            <GuestRoute>
-              <LoginPage />
-            </GuestRoute>
-          }
-        />
-        <Route
-          path="/register"
-          element={
-            <GuestRoute>
-              <RegisterPage />
-            </GuestRoute>
-          }
-        />
-        <Route
-          path="/forgot-password"
-          element={
-            <GuestRoute>
-              <ForgotPasswordPage />
-            </GuestRoute>
-          }
-        />
-
-        <Route
-          path="/dashboard"
-          element={
-            <ProtectedRoute>
-              <DashboardPage />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route path="/activate/:uid/:token" element={<ActivationSuccess />} />
-
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          {routes.map(({ path, element }) => (
+            <Route key={path} path={path} element={element} />
+          ))}
+        </Routes>
+      </Suspense>
 
       <Footer />
     </>
