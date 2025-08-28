1aaab5126f8187bf294ae9515ac43fd1e07388b8
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2025-08-28 14:43:26 +0300

    Update PricingButton.jsx

--- client/MacroMaster/src/pages/Pricing/components/PricingButton.jsx
+++ client/MacroMaster/src/pages/Pricing/components/PricingButton.jsx
@@ -1,10 +1,12 @@
+import { pricingStyles } from "../styles";
+
 export default function PriceButton() {
-	return (
-		<button
-			className="px-4 py-3 w-full rounded-lg text-sm font-semibold text-white bg-gray-400 disabled:cursor-not-allowed disabled:bg-gray-400 hover:bg-gray-500 active:bg-gray-600 mt-4"
-			disabled
-		>
-			Unavailable
-		</button>
-	);
+  return (
+    <button
+      className={`${pricingStyles.button} ${pricingStyles.buttonDisabled} ${pricingStyles.buttonHover} ${pricingStyles.buttonActive}`}
+      disabled
+    >
+      Unavailable
+    </button>
+  );
 }
