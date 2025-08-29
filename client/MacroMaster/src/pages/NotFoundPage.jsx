66f5d423f3d75ccfc2eb2c58dfd646b00994942a
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2025-08-29 16:36:08 +0300

    Update NotFoundPage.jsx

--- client/MacroMaster/src/pages/NotFoundPage.jsx
+++ client/MacroMaster/src/pages/NotFoundPage.jsx
@@ -3,8 +3,17 @@ import { commonStyles } from "../styles/commonStyles";
 
 export default function NotFoundPage() {
   return (
-    <div className={`${commonStyles.container} flex items-center justify-center`}>
-      <div className={`${commonStyles.card} text-center p-8`}>
+    <div className={`${commonStyles.container} grid place-items-center`}>
+      {/* Gradient blur background */}
+      <div
+        className={commonStyles.gradientBlur}
+        style={{
+          background:
+            "linear-gradient(106.89deg, rgba(192, 132, 252, 0.2) 15.73%, rgba(14, 165, 233, 0.6) 15.74%, rgba(232, 121, 249, 0.35) 56.49%, rgba(79, 70, 229, 0.5) 115.91%)",
+        }}
+      />
+
+      <div className={`${commonStyles.card} text-center p-8 relative z-10`}>
         <h1 className="text-6xl md:text-7xl font-bold text-gray-800 dark:text-gray-100 mb-4">
           404
         </h1>
