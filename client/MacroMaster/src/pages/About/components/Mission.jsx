1b876db07e44db65bce9a4452d22591503b9ac80
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2025-08-30 15:41:28 +0300

    Update Mission.jsx

--- client/MacroMaster/src/pages/About/components/Mission.jsx
+++ client/MacroMaster/src/pages/About/components/Mission.jsx
@@ -1,9 +1,10 @@
+import { styles } from "../styles";
 
 export default function Mission() {
   return (
-    <div className="mt-16 max-w-2xl text-center">
-      <h2 className="text-3xl font-bold mb-4">Our Mission</h2>
-      <p className="text-gray-700 dark:text-gray-300 text-lg leading-relaxed">
+    <div className={styles.mission.container}>
+      <h2 className={styles.mission.heading}>Our Mission</h2>
+      <p className={styles.mission.text}>
         We aim to empower people to make informed nutritional choices,
         simplify tracking, and achieve their health goals with confidence.
       </p>
