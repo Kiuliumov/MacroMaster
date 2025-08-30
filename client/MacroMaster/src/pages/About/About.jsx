6fad3ec5e2a9dc98c58522dea4d8624d79101c08
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2025-08-30 15:41:21 +0300

    Improvem

--- client/MacroMaster/src/pages/About/About.jsx
+++ client/MacroMaster/src/pages/About/About.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Header from "./components/Header";
 import FeaturesGrid from "./components/FeaturesGrid";
 import Mission from "./components/Mission";
@@ -6,16 +5,14 @@ import ValuesGrid from "./components/ValuesGrid";
 import TestimonialsGrid from "./components/TestimonialGrid";
 import CTA from "./components/CTA";
 import { commonStyles } from "../../styles/commonStyles";
+import { styles } from "./styles";
 
 export default function About() {
   return (
     <div className={commonStyles.container}>
       <div
         className={commonStyles.gradientBlur}
-        style={{
-          background:
-            "linear-gradient(106.89deg, rgba(192, 132, 252, 0.2) 15.73%, rgba(14, 165, 233, 0.6) 15.74%, rgba(232, 121, 249, 0.35) 56.49%, rgba(79, 70, 229, 0.5) 115.91%)",
-        }}
+        style={styles.about.gradientBlur}
       />
 
       <div className="relative max-w-5xl mx-auto space-y-20 flex flex-col items-center w-full">
