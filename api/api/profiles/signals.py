c324301a0abc40269ec77368b0e44189fb14ce2c
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2025-09-02 14:20:54 +0300

    Authentication

--- api/api/profiles/signals.py
+++ api/api/profiles/signals.py
@@ -1,8 +1,6 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-
 from .models import UserModel, Profile
-
 
 @receiver(post_save, sender=UserModel)
 def create_user_profile(sender, instance, created, **kwargs):
