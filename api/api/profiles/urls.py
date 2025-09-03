598fd24fe7f6fa0377d900a354cbb3dbbfdb8d22
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2025-09-03 08:27:11 +0300

    Update urls.py

--- api/api/profiles/urls.py
+++ api/api/profiles/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path("activate/<uidb64>/<token>/", ActivateAccountView.as_view(), name="activate"),
     path("check_email/", views.CheckRegisteredEmails.as_view(), name="check_email"),
     path("check_username/", views.CheckRegisteredUsernames.as_view(), name="check_username"),
+    path("me/", views.MeView.as_view(), name="me"),
 ]
