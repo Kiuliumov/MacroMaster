fae82434899c28403de05c8764795b2c952e32ec
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2025-09-03 08:27:14 +0300

    Update useAuth.js

--- client/MacroMaster/hooks/useAuth.js
+++ client/MacroMaster/hooks/useAuth.js
@@ -1,24 +1,11 @@
-import { useSelector, useDispatch } from "react-redux";
-import { useEffect } from "react";
-import { getJwtFromCookie } from "../authentication";
-import { setUser, logout } from "../src/state_manager/userSlice";
+import { useSelector } from "react-redux";
 
 export function useAuth() {
-  const dispatch = useDispatch();
   const user = useSelector((state) => state.user.user);
-  const token = getJwtFromCookie("access");
-  const isLoggedIn = !!token;
+  const isLoggedIn = useSelector((state) => state.user.isLoggedIn);
+
+  const token = user?.token || null;
   const onboardingRequired = isLoggedIn && user && !user.onboardingCompleted;
 
-  useEffect(() => {
-    if (token && !user) {
-      dispatch(setUser({ token, onboardingCompleted: false }));
-    }
-
-    if (!token && user) {
-      dispatch(logout());
-    }
-  }, [token, user, dispatch]);
-
-  return { user, isLoggedIn, token, onboardingRequired };
+  return { user, isLoggedIn, token, onboardingRequired, loading: false };
 }
