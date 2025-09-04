86e8e182409feaaf1a8eaee16311c432c6bb3a33
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2025-09-04 16:50:20 +0300

    Update urls.py

--- api/api/profiles/urls.py
+++ api/api/profiles/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from . import views
-from .views import ActivateAccountView
+from .views import ActivateAccountView, ForgotPasswordView, ResetPasswordView
 
 urlpatterns = [
     path('register/', views.RegisterView.as_view(), name='register'),
@@ -9,5 +9,7 @@ urlpatterns = [
     path("activate/<uidb64>/<token>/", ActivateAccountView.as_view(), name="activate"),
     path("check_email/", views.CheckRegisteredEmails.as_view(), name="check_email"),
     path("check_username/", views.CheckRegisteredUsernames.as_view(), name="check_username"),
+    path("forgot-password/", ForgotPasswordView.as_view(), name="forgot-password"),
+    path("reset-password/", ResetPasswordView.as_view(), name="reset-password"),
     path("me/", views.MeView.as_view(), name="me"),
 ]
