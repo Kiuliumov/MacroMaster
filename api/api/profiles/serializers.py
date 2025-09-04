db1883e80045f09af1cd78893fc1cdd36a0128f1
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2025-09-04 16:50:19 +0300

    Update serializers.py

--- api/api/profiles/serializers.py
+++ api/api/profiles/serializers.py
@@ -63,3 +63,36 @@ class ForgotPasswordSerializer(serializers.Serializer):
         except User.DoesNotExist:
             raise serializers.ValidationError("User with this email does not exist.")
         return value
+
+from rest_framework import serializers
+from django.utils.encoding import force_str
+from django.utils.http import urlsafe_base64_decode
+from django.contrib.auth.tokens import default_token_generator
+
+class ResetPasswordSerializer(serializers.Serializer):
+    uid = serializers.CharField()
+    token = serializers.CharField()
+    new_password = serializers.CharField(write_only=True, min_length=8)
+
+    def validate(self, attrs):
+        uid = attrs.get("uid")
+        token = attrs.get("token")
+
+        try:
+            user_id = force_str(urlsafe_base64_decode(uid))
+            user = User.objects.get(pk=user_id)
+        except (User.DoesNotExist, ValueError, TypeError):
+            raise serializers.ValidationError({"uid": "Invalid UID"})
+
+        if not default_token_generator.check_token(user, token):
+            raise serializers.ValidationError({"token": "Invalid or expired token"})
+
+        attrs["user"] = user
+        return attrs
+
+    def save(self, **kwargs):
+        password = self.validated_data["new_password"]
+        user = self.validated_data["user"]
+        user.set_password(password)
+        user.save()
+        return user
