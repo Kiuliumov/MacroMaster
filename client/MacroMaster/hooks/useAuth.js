2a10ab576b8de880e22daf69b67a5781b16c23c3
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2025-09-12 14:43:17 +0300

    Update useAuth.js

--- client/MacroMaster/hooks/useAuth.js
+++ client/MacroMaster/hooks/useAuth.js
@@ -3,9 +3,16 @@ import { useSelector } from "react-redux";
 export function useAuth() {
   const user = useSelector((state) => state.user.user);
   const isLoggedIn = useSelector((state) => state.user.isLoggedIn);
+  const accessToken = useSelector((state) => state.user.accessToken);
 
-  const token = user?.token || null;
-  const onboardingRequired = isLoggedIn && user && !user.onboardingCompleted;
+  const onboardingRequired =
+    isLoggedIn && user && !user.stats?.daily_calories;
 
-  return { user, isLoggedIn, token, onboardingRequired, loading: false };
+  return {
+    user,
+    isLoggedIn,
+    token: accessToken,
+    onboardingRequired,
+    loading: false,
+  };
 }
