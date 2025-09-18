817071111fc0b81cc3a20858afbfaaa63451aa71
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2025-09-18 18:31:39 +0300

    Update Button.jsx

--- client/MacroMaster/src/components/Button.jsx
+++ client/MacroMaster/src/components/Button.jsx
@@ -1,9 +1,20 @@
-export default function Button({ children, className = "", ...props }) {
+import { Link } from "react-router-dom";
+
+export default function Button({ children, to, className = "", ...props }) {
+  const baseClass =
+    "inline-flex items-center px-4 py-2 text-sm font-semibold rounded-md shadow transition-all duration-200 " +
+    className;
+
+  if (to) {
+    return (
+      <Link to={to} className={baseClass} {...props}>
+        {children}
+      </Link>
+    );
+  }
+
   return (
-    <button
-      className={`inline-flex items-center px-4 py-2 text-sm font-semibold rounded-md shadow transition-all duration-200 ${className}`}
-      {...props}
-    >
+    <button className={baseClass} {...props}>
       {children}
     </button>
   );
