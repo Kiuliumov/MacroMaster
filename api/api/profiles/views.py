7b508917f212d4d741cacf19be64b916acfbf9ee
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2025-09-18 18:31:36 +0300

    Update views.py

--- api/api/profiles/views.py
+++ api/api/profiles/views.py
@@ -1,8 +1,5 @@
 from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import send_mail
-from django.shortcuts import render
-from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
-from django.utils.encoding import force_bytes, force_str
 
 # Create your views here.
 from rest_framework.views import APIView
