8a6768816b0c09e0dab6de8a124e3a5422298471
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2025-09-18 19:36:09 +0300

    Update models.py

--- api/api/profiles/models.py
+++ api/api/profiles/models.py
@@ -14,6 +14,7 @@ class Profile(models.Model):
     gender = models.CharField(max_length=10, null=True, blank=True)
     height_cm = models.FloatField(null=True, blank=True)
     weight_kg = models.FloatField(null=True, blank=True)
+    onboarding_completed = models.BooleanField(default=False)
     activity_level = models.CharField(
         max_length=20,
         choices=[
