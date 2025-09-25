7ec9c34ed1dd30fbfec5f8f0230c4ee643e94d38
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2025-09-25 20:51:25 +0300

    Update Homepage.jsx

--- client/MacroMaster/src/pages/Home/Homepage.jsx
+++ client/MacroMaster/src/pages/Home/Homepage.jsx
@@ -1,24 +1,30 @@
 import { commonStyles } from "../../styles/commonStyles";
-import HeroSection from "./components/HeroSection";
 import FeaturesSection from "./components/FeaturesSection";
+import Logo from '../../components/Logo';
 import { useAuth } from "../../../hooks/useAuth";
 
 export default function Homepage() {
-  const { isLoggedIn } = useAuth();   
-  return (
-    <main className={commonStyles.container}>
-      <div
-        className={commonStyles.gradientBlur}
-        style={{
-          background:
-            "linear-gradient(106.89deg, rgba(192, 132, 252, 0.2) 15.73%, rgba(14, 165, 233, 0.6) 15.74%, rgba(232, 121, 249, 0.35) 56.49%, rgba(79, 70, 229, 0.5) 115.91%)",
-        }}
-      />
+	const { isLoggedIn, user} = useAuth();
+	return (
+		<main className={commonStyles.container}>
+			<div
+				className={commonStyles.gradientBlur}
+				style={{
+					background:
+						"linear-gradient(106.89deg, rgba(192, 132, 252, 0.2) 15.73%, rgba(14, 165, 233, 0.6) 15.74%, rgba(232, 121, 249, 0.35) 56.49%, rgba(79, 70, 229, 0.5) 115.91%)",
+				}}
+			/>
 
-      <div className="relative max-w-5xl mx-auto space-y-20 flex flex-col items-center">
-        <HeroSection />
-        {!isLoggedIn && <FeaturesSection />}
-      </div>
-    </main>
-  );
+			<div className="relative max-w-5xl mx-auto space-y-20 flex flex-col items-center">
+				<div className={commonStyles.headerWrapper}>
+					<div className="flex justify-center mb-6">
+						<Logo className="h-16 w-16 animate-fade-in" />
+					</div>
+
+					{isLoggedIn ? <AccountHero user={user} /> : <NoAccountHero />}
+				</div>
+				{!isLoggedIn && <FeaturesSection />}
+			</div>
+		</main>
+	);
 }
