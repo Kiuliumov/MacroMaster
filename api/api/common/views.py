0f52b2a96417f2a67d8a4f71dc9f15edd021be20
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2025-10-06 19:04:25 +0300

    Create views.py

---
+++ api/api/common/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework import generics, permissions
+from .models import ContactMessage
+from .serializers import ContactMessageSerializer
+from permissions import IsJWTAdmin
+
+class ContactMessageListCreateView(generics.ListCreateAPIView):
+    """
+    GET: List all contact messages
+    POST: Submit a new contact message
+    """
+    queryset = ContactMessage.objects.all().order_by('-id')
+    serializer_class = ContactMessageSerializer
+    permission_classes = [IsJWTAdmin]
