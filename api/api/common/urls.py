81a9fb025b2f4f58ef538ade6854fe344dfdefe8
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2025-10-06 19:53:14 +0300

    Update urls.py

--- api/api/common/urls.py
+++ api/api/common/urls.py
@@ -1,7 +1,8 @@
+# urls.py
 from django.urls import path
-
-from .views import ContactMessageListCreateView
+from .views import ContactMessageListCreateView, ContactMessageDetailView
 
 urlpatterns = [
-    path('contact-messages/', ContactMessageListCreateView.as_view(), name='contact-messages-create'),
+    path('contact-messages/', ContactMessageListCreateView.as_view(), name='messages-list-create'),
+    path('contact-messages/<int:pk>/', ContactMessageDetailView.as_view(), name='messages-detail-delete'),
 ]
