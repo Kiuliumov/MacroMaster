8dee5ac875609db46cbd46b0dbac35d8ff6c294a
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2025-10-06 19:41:34 +0300

    Update models.py

--- api/api/common/models.py
+++ api/api/common/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 class ContactMessage(models.Model):
     name = models.CharField(max_length=100)
+    subject = models.CharField(max_length=100)
     email = models.EmailField()
     message = models.TextField()
 
