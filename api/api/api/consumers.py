f3a8616c7acf5833818cfd74203b7e1180716d24
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2025-10-07 09:03:42 +0300

    Update consumers.py

--- api/api/api/consumers.py
+++ api/api/api/consumers.py
@@ -1,5 +1,8 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.db import database_sync_to_async
+from .models import ContactMessage
+from .serializers import ContactMessageSerializer
 
 class ContactMessageConsumer(AsyncWebsocketConsumer):
     async def connect(self):
@@ -12,20 +15,30 @@ class ContactMessageConsumer(AsyncWebsocketConsumer):
 
     async def receive(self, text_data):
         data = json.loads(text_data)
-        action = data.get("action")
-        message = data.get("message")
 
-        await self.channel_layer.group_send(
-            self.group_name,
-            {
-                "type": "broadcast_message",
-                "action": action,
-                "message": message,
-            }
-        )
+        # Handle deletion
+        if data.get("action") == "delete":
+            msg_id = data.get("id")
+            if msg_id:
+                deleted_msg = await self.delete_message(msg_id)
+                if deleted_msg:
+                    # Broadcast deletion to all clients
+                    await self.channel_layer.group_send(
+                        self.group_name,
+                        {
+                            "type": "broadcast_message",
+                            "data": {"action": "deleted", "message": {"id": msg_id}},
+                        }
+                    )
 
     async def broadcast_message(self, event):
-        await self.send(text_data=json.dumps({
-            "action": event["action"],
-            "message": event["message"],
-        }))
+        await self.send(text_data=json.dumps(event["data"]))
+
+    @database_sync_to_async
+    def delete_message(self, msg_id):
+        try:
+            msg = ContactMessage.objects.get(id=msg_id)
+            msg.delete()
+            return True
+        except ContactMessage.DoesNotExist:
+            return False
