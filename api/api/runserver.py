1ed5ace048be998352cf66aa5942ab523a769606
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2025-10-07 08:50:41 +0300

    Command line run configuration for daphne

--- api/api/runserver.py
+++ api/api/runserver.py
@@ -1,12 +1,20 @@
 # run_django_server.py
 import os
 import sys
-from django.core.management import execute_from_command_line
+from daphne.cli import CommandLineInterface
 
 def main():
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "api.settings")
-    sys.argv = ["manage.py", "runserver"]
-    execute_from_command_line(sys.argv)
+
+    # Define Daphne arguments
+    sys.argv = [
+        "daphne",                    # command
+        "-p", "8000",                # port
+        "api.asgi:application",      # ASGI application path
+    ]
+
+    # Run Daphne CLI
+    CommandLineInterface.entrypoint()
 
 if __name__ == "__main__":
     main()
