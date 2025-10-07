1a2121a43c4e93d22dd278b6e2bcc8b8724768ba
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2025-10-07 09:06:31 +0300

    Update routing.py

--- api/api/api/routing.py
+++ api/api/api/routing.py
@@ -2,5 +2,5 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r"ws/contact-messages/$", consumers.ContactMessageConsumer.as_asgi()),
+    re_path(r"api/ws/contact-messages/$", consumers.ContactMessageConsumer.as_asgi()),
 ]
