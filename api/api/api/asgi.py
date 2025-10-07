2e7074616176bc8b0a0a0be2cb19328b0d0576a1
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2025-10-07 09:03:39 +0300

    Update asgi.py

--- api/api/api/asgi.py
+++ api/api/api/asgi.py
@@ -2,15 +2,15 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from . import routing
+import api.routing
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "your_project_name.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CalorieTrackerApp.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
         URLRouter(
-            routing.websocket_urlpatterns
+            api.routing.websocket_urlpatterns
         )
     ),
 })
