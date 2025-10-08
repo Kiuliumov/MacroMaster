79559cf24f3c7776ec1da764a093bd5220a7a243
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2025-10-08 09:43:02 +0300

    Update ProtectedRoute.jsx

--- client/MacroMaster/src/route_components/ProtectedRoute.jsx
+++ client/MacroMaster/src/route_components/ProtectedRoute.jsx
@@ -1,12 +1,16 @@
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../../hooks/useAuth";
+import { useDispatch } from "react-redux";
+import { addToast } from "../state_manager/toastSlice";
 
 export default function ProtectedRoute({ children }) {
   const { user, loading } = useAuth();
+  const dispatch = useDispatch();
 
   if (loading) return null; 
 
   if (!user) {
+    dispatch(addToast({ message: "You need to log in to access this page.", type: "warning" }));
     return <Navigate to="/login" replace />;
   }
 
