bed7e1670c8886afcaeed6ef57c28ec64150142c
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2025-10-08 09:42:59 +0300

    Update GuestRoute.jsx

--- client/MacroMaster/src/route_components/GuestRoute.jsx
+++ client/MacroMaster/src/route_components/GuestRoute.jsx
@@ -1,10 +1,22 @@
+import { useEffect, useRef } from "react";
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../../hooks/useAuth";
+import { useDispatch } from "react-redux";
+import { addToast } from "../state_manager/toastSlice";
 
 export default function GuestRoute({ children }) {
   const { user, loading } = useAuth();
+  const dispatch = useDispatch();
+  const toastFired = useRef(false);
 
-  if (loading) return null; 
+  useEffect(() => {
+    if (!toastFired.current && user) {
+      dispatch(addToast({ message: "You are already logged in.", type: "danger" }));
+      toastFired.current = true;
+    }
+  }, [user, dispatch]);
+
+  if (loading) return null;
 
   if (user) {
     return <Navigate to="/" replace />;
