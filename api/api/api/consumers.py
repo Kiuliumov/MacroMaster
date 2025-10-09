edb86dae45b357a08898997b638e62db9ba48d2b
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2025-10-09 09:23:31 +0300

    Update consumers.py

--- api/api/api/consumers.py
+++ api/api/api/consumers.py
@@ -1,5 +1,7 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
+from asgiref.sync import sync_to_async
+
 
 class ContactMessageConsumer(AsyncWebsocketConsumer):
     async def connect(self):
@@ -13,19 +15,42 @@ class ContactMessageConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data):
         data = json.loads(text_data)
         action = data.get("action")
-        message = data.get("message")
+        print(data)
+
+        if action == "create":
+            await self.create_contact_message(data)
 
         await self.channel_layer.group_send(
             self.group_name,
             {
                 "type": "broadcast_message",
                 "action": action,
-                "message": message,
+                "message": data.get("message"),
             }
         )
 
     async def broadcast_message(self, event):
-        await self.send(text_data=json.dumps({
-            "action": event["action"],
-            "message": event["message"],
-        }))
+        await self.send(
+            text_data=json.dumps(
+                {
+                    "action": event["action"],
+                    "message": event["message"],
+                }
+            )
+        )
+
+    @sync_to_async
+    def create_contact_message(self, data):
+        from common.models import ContactMessage
+
+        name = data.get("name", "")
+        email = data.get("email", "")
+        subject = data.get("subject", "")
+        message = data.get("message", "")
+
+        return ContactMessage.objects.create(
+            name=name,
+            email=email,
+            subject=subject,
+            message=message,
+        )
